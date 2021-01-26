a97a9c075502c01a080191dff9028957f6d18b84
cbscar <cbscar@gmail.com>
2021-01-26 10:56:59 -0600

    fixed APP_ID reference

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const DOMAIN = 'https://api.edamam.com/';
-const APP_ID = '52f197ec'
-const APP_KEY = 'ab07ec683eee2c0e4a1bf835c068fd6b'
-const BASE_URL = `${DOMAIN}?app_id=${API_KEY}&app_key=${}`;
+const APP_ID = '52f197ec';
+const APP_KEY = 'ab07ec683eee2c0e4a1bf835c068fd6b';
+const BASE_URL = `${DOMAIN}?app_id=${APP_ID}&app_key=${APP_KEY}`;
+
